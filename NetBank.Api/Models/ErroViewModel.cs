6e5bfaab592823739ea1c7d6de828f48e78bc992
João Alencar <joao.alencar1497@outlook.com>
2023-04-16 16:44:18 -0300

    ErroViewModel padrão para envio de erros na API

---
+++ NetBank.Api/Models/ErroViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace NetBank.Api.Models
+{
+    public class ErroViewModel
+    {
+        public string Mensagem { get; set; }
+
+        public string? StackTrace { get; set; }
+
+        public ErroViewModel(string mensagem, string stackTrace = null)
+        {
+            Mensagem = mensagem;
+            StackTrace = stackTrace;
+
+        }
+    }
+}

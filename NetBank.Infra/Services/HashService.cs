c06c3e45129ea8aa7462e2c913d4ba1a0a04a4cb
João Alencar <joao.alencar1497@outlook.com>
2023-04-16 16:07:26 -0300

    criado serviços de hash e geração de token do usuario

---
+++ NetBank.Infra/Services/HashService.cs
@@ -0,0 +1,14 @@
+﻿using static BCrypt.Net.BCrypt;
+
+namespace NetBank.Infra.Services
+{
+    public static class HashService
+    {
+        public static string HashSenha(string senha)
+            => HashPassword(senha);
+
+
+        public static bool ComparaSenha(string senha, string hash)
+            => Verify(senha, hash);
+    }
+}
---
+++ NetBank.Infra/Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NetBank.Domain.Entidades;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NetBank.Infra.Services
+{
+    public static class TokenService
+    {
+        public static string GerarTokenUsuario(Usuario usuario)
+        {
+            var key = Environment.GetEnvironmentVariable("JwtKey")!;
+            var bytesKey = Encoding.ASCII.GetBytes(key);
+
+            var tokenHanlder = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.Now.AddMinutes(10),
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new(ClaimTypes.Email, usuario.Email),
+                    new(ClaimTypes.Name, usuario.NomeCompleto),
+                    new(ClaimTypes.OtherPhone, usuario.Telefone),
+                    new("Id", usuario.Id)
+                }),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(bytesKey), SecurityAlgorithms.HmacSha256),
+            };
+
+            var token = tokenHanlder.CreateToken(tokenDescriptor);
+
+            return tokenHanlder.WriteToken(token);
+        }
+    }
+}

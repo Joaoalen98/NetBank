81eaa0fcd23dcc8288e336c057eb8d1a267cc54a
João Alencar <joao.alencar1497@gmail.com>
2023-04-17 11:34:50 -0300

    adicionado names ao enum de tipo de conta

--- NetBank.Domain/Enums/TipoContaEnum.cs
+++ NetBank.Domain/Enums/TipoContaEnum.cs
@@ -1,9 +1,13 @@
-﻿namespace NetBank.Domain.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetBank.Domain.Enums
 {
     public enum TipoContaEnum
     {
+        [Display(Name = "Conta Corrente")]
         ContaCorrente = 1,
 
+        [Display(Name = "Conta Poupança")]
         ContaPoupanca = 2,
     }
 }

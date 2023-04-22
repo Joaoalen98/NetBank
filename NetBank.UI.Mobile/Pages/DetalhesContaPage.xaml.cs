d02abe2c2dcf254aa91ad921c092faa23d720261
João Alencar <joao.alencar1497@outlook.com>
2023-04-21 23:02:11 -0300

    feat: DetalhesConta - criado pagina de detalhes da conta

---
+++ NetBank.UI.Mobile/Pages/DetalhesContaPage.xaml.cs
@@ -0,0 +1,55 @@
+using NetBank.Domain.Entidades;
+using NetBank.UI.Mobile.Services;
+
+namespace NetBank.UI.Mobile.Pages;
+
+public partial class DetalhesContaPage : ContentPage
+{
+    ApiService _apiService;
+
+    public Conta Conta;
+
+    public DetalhesContaPage(Conta conta)
+    {
+        Conta = conta;
+
+
+        InitializeComponent();
+        BindingContext = Conta;
+
+        _apiService = new ApiService();
+
+        datePickerDataInicial.Date = DateTime.Now.AddMonths(-1);
+        datePickerDataFinal.Date = DateTime.Now;
+    }
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        
+
+        listTransacoes.ItemsSource = await GetTransacoes();
+    }
+
+
+    private async Task<IEnumerable<Transacao>> GetTransacoes()
+    {
+        var dataInicial = datePickerDataInicial.Date;
+        var dataFinal = datePickerDataFinal.Date;
+
+        return await _apiService.GetTransacoesConta(
+            Conta.Id, dataInicial, dataFinal);
+    }
+
+    private async void btnPesquisar_Clicked(object sender, EventArgs e)
+    {
+        listTransacoes.ItemsSource = await GetTransacoes();
+    }
+
+    private void btnComprovante_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}

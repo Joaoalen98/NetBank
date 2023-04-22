5dd984445e93cc49cc6f36688a944ad8d2f75466
João Alencar <joao.alencar1497@outlook.com>
2023-04-21 23:01:40 -0300

    feat: App definido HomePage como rota inicial do aplicativo

--- NetBank.UI.Mobile/App.xaml.cs
+++ NetBank.UI.Mobile/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace NetBank.UI.Mobile
+﻿using NetBank.UI.Mobile.Pages;
+
+namespace NetBank.UI.Mobile
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new HomePage());
         }
     }
 }

46e988992959f9f50fdb5da84c358788304837cb
João Alencar <joao.alencar1497@outlook.com>
2023-04-21 22:05:47 -0300

    feat: solução - adicionado projeto .net maui à solução

---
+++ NetBank.UI.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NetBank.UI.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

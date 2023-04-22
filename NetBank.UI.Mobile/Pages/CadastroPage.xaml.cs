c11a176be5f102f611da2d7d80dd001514883398
João Alencar <joao.alencar1497@outlook.com>
2023-04-21 22:59:39 -0300

    feat: Cadastro - criado página de cadastro

---
+++ NetBank.UI.Mobile/Pages/CadastroPage.xaml.cs
@@ -0,0 +1,45 @@
+using NetBank.DTOs;
+using NetBank.UI.Mobile.Services;
+
+namespace NetBank.UI.Mobile.Pages;
+
+public partial class CadastroPage : ContentPage
+{
+    ApiService _apiService;
+
+    public CadastroPage()
+    {
+        InitializeComponent();
+        datePickerDataNascimento.Date = DateTime.Now.AddYears(-18);
+
+        _apiService = new ApiService();
+    }
+
+    private async void btnCadastrar_Clicked(object sender, EventArgs e)
+    {
+        if (entrySenha.Text != entryConfirmacaoSenha.Text)
+        {
+            await DisplayAlert("Informações incorretas", "As duas senhas não informadas não são iguais", "Fechar");
+        }
+
+        var usuarioDTO = new UsuarioDTO()
+        {
+            CPF = entryCPF.Text,
+            DataNascimento = datePickerDataNascimento.Date,
+            Email = entryEmail.Text,
+            NomeCompleto = entryNome.Text,
+            Senha = entrySenha.Text,
+            Telefone = entryTelefone.Text,
+        };
+
+        try
+        {
+            await _apiService.CadastrarUsuario(usuarioDTO);
+            await Navigation.PushAsync(new LoginPage());
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro ao cadastrar", ex.Message, "Fechar");
+        }
+    }
+}
